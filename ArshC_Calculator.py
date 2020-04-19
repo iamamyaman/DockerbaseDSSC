a58b1f808531b5cfc146549638ae3340431bde7b
rohitsingla2004 <63734167+rohitsingla2004@users.noreply.github.com>
2020-04-19 14:10:41 +0530

    Add files via upload

---
+++ ArshC_Calculator.py
@@ -0,0 +1,25 @@
+print("select operation")
+print("1:add")
+print("2:subtract")
+print("3:multiply")
+print("4:divide")
+
+choice = input("enter choice:")
+
+num1 = float(input("enter first number:"))
+num2 = float(input("enter second number:"))
+
+if choice == "1":
+    print(num1, "+", num2, "=", (num1+num2))
+
+elif choice == "2":
+    print(num1, "-", num2, "=", (num1-num2))
+
+elif choice == "3":
+    print(num1, "*", num2, "=", (num1*num2))
+
+elif choice == "4":
+    print(num1, "/", num2, "=", (num1/num2))
+
+else:
+    print("invalid input")
